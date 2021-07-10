80794f5d317b4037cd093d13ff164c8c896c32f9
DESKTOP-9RO8RKP\infinity <baldoswill@gmail.com>
2021-07-10 02:46:19 -0700

    Added Migrations and Seed

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TourWebApi.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Philippines",
+                    ShortName = "PH"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "United States of America",
+                    ShortName = "USA"
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.1
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 3.1
+                }
+            );
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using TourWebApi.Data;
 
 namespace TourWebApi
 {
@@ -27,6 +29,10 @@ namespace TourWebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("psqlConnection"));
+            });
             services.AddCors(o =>
             {
                 o.AddPolicy("AllowAll", builder => builder
